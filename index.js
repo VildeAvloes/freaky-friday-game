9e05ab6dfd6308bbaf28e44f4e55342d872900de
VildeAvloes <94361976+VildeAvloes@users.noreply.github.com>
2023-01-17 13:54:13 +0100

    deleted code and started from the beginning

--- index.js
+++ index.js
@@ -1,60 +1,34 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-//gameboard
 canvas.width = 1024;
 canvas.height = 576;
 
-const scaledCanvas = {
-  width: canvas.width / 4,
-  height: canvas.height / 4,
-};
-
-//collision blocks
-const floorCollisions2D = [];
-for (let i = 0; i < floorCollisions.length; i += 36) {
-  floorCollisions2D.push(floorCollisions.slice(i, i + 36));
-}
-
-const collisionBlocks = [];
-floorCollisions2D.forEach((row, y) => {
-  row.forEach((symbol, x) => {
-    if (symbol === 202) {
-      collisionBlocks.push(
-        new collisionBlock({
-          position: {
-            x: x * 16,
-            y: y * 16,
-          },
-        })
-      );
-    }
-  });
-});
-
-const platformCollisions2D = [];
-for (let i = 0; i < platformCollisions.length; i += 36) {
-  platformCollisions2D.push(platformCollisions.slice(i, i + 36));
-}
-const platformCollisionBlocks = [];
-platformCollisions2D.forEach((row, y) => {
-  row.forEach((symbol, x) => {
-    if (symbol === 202) {
-      platformCollisionBlocks.push(
-        new collisionBlock({
-          position: {
-            x: x * 16,
-            y: y * 16,
-          },
-        })
-      );
-    }
-  });
-});
-
-console.log(platformCollisions2D);
-
 const gravity = 0.5;
+
+class Player {
+  constructor(postion) {
+    this.position = postion;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.height = 100;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 100, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+    if (this.position.y + this.height + this.velocity.y < canvas.height) this.velocity.y += gravity;
+    else this.velocity.y = 0;
+  }
+}
 
 const player = new Player({
   x: 0,
@@ -74,34 +48,10 @@ const keys = {
   },
 };
 
-const background = new Sprite({
-  position: {
-    x: 0,
-    y: 0,
-  },
-  imageSrc: "./img/background.png",
-});
-
-//animation
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
-
-  c.save();
-  c.scale(4, 4);
-  c.translate(0, -background.image.height + scaledCanvas.height);
-  background.update();
-  collisionBlocks.forEach((collisionBlock) => {
-    collisionBlock.update();
-  });
-
-  platformCollisionBlocks.forEach((block) => {
-    block.update();
-  });
-
-  c.restore();
-
   player.update();
   player2.update();
 
@@ -112,31 +62,26 @@ function animate() {
 
 animate();
 
-//keyboard navigation for player
+//player movement
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
-    //right
     case "d":
       keys.d.pressed = true;
       break;
-    //left
     case "a":
       keys.a.pressed = true;
       break;
-    //jump
     case "w":
-      player.velocity.y = -15;
+      player.velocity.y = -20;
       break;
   }
 });
 
 window.addEventListener("keyup", (event) => {
   switch (event.key) {
-    //right
     case "d":
       keys.d.pressed = false;
       break;
-    //left
     case "a":
       keys.a.pressed = false;
       break;
